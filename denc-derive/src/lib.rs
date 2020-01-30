6b898500db34ea22a90083a634b2b3581ab6ba92
Vova <vova616@gmail.com>
2020-01-30 14:25:01 +0200

    current state

---
+++ denc-derive/src/lib.rs
@@ -0,0 +1,178 @@
+#![feature(generators, generator_trait)]
+#![recursion_limit = "128"]
+
+extern crate proc_macro;
+
+use self::proc_macro::TokenStream;
+use quote::quote;
+use quote::ToTokens;
+use syn::parse_quote;
+
+extern crate proc_macro2;
+use self::proc_macro2::TokenStream as TokenStream2;
+use std::collections::HashSet;
+use syn::{ImplGenerics, Generics};
+use syn::parse::Parse;
+
+#[proc_macro_derive(MapperDec)]
+pub fn derive_mapper_dec(input: TokenStream) -> TokenStream {
+    let mut input: syn::ItemStruct = syn::parse(input).unwrap();
+
+    let mut types: Vec<syn::Type> = input
+        .fields
+        .iter()
+        .enumerate()
+        .map(|(i, f)| f.ty.clone())
+        .collect();
+    
+    let set: HashSet<syn::Type> = types.iter().map(|t| t.clone()).collect(); // dedup
+    let types_uniq = set.into_iter();
+
+    //let types_uniq = types.so    
+    let decoder_decode_impl = input.fields.iter().enumerate().map(|(i, f)| {
+        let name = f.ident.as_ref().unwrap();
+        let ty = &f.ty;
+
+        let concatenated = format!("_dec_{}", name);
+        let size_name = syn::Ident::new(&concatenated, name.span());
+        let inner_types = types.iter().skip(i);
+       
+        if i == 0 {
+            quote! {
+                if decoder.len() < <#ty as Decode<Dec>>::SIZE {
+                    return Err(Dec::EOF);
+                }
+                let #name = <#ty as Decode<Dec>>::decode(decoder)?;
+            }
+        } else {
+            let prev_type = types.get(i - 1).unwrap();
+            quote! {
+                if !<#prev_type as Decode<Dec>>::STATIC && decoder.len() < <#ty as Decode<Dec>>::SIZE {
+                    return Err(Dec::EOF);
+                }
+                let #name = <#ty as Decode<Dec>>::decode(decoder)?;
+            }
+        }
+    });
+    let decoder_decode_return_impl = input.fields.iter().enumerate().map(|(i, f)| {
+        let name = &f.ident;
+        quote! {
+           #name
+        }
+    });
+    let name = &input.ident;
+    let attrs = &input.attrs;
+
+    let mut generics_clone = input.generics.clone();
+    generics_clone.params.push(parse_quote!{ Dec: Decoder });
+    let (impl_generics, _, _) = generics_clone.split_for_impl();
+
+    let (_, ty_generics, where_clause) = input.generics.split_for_impl();
+
+ 
+    let output = quote! {
+        impl #impl_generics Decode<Dec> for #name #ty_generics
+            where 
+            #(
+                #types_uniq : Decode<Dec>
+            ),*
+        
+        {
+            const SIZE: usize = #(
+                <#types as Decode<Dec>>::SIZE
+             )+*;
+
+            #[inline(always)]
+            fn decode(decoder: &mut Dec) -> Result<Self, Dec::Error>  {
+                //decoder.fill_buffer(<#name as Decode<Dec>>::SIZE);
+
+                #(
+                    #decoder_decode_impl
+                )*
+
+                Ok(#name {
+                    #(
+                        #decoder_decode_return_impl,
+                    )*
+                })
+            }
+        }
+    };
+
+    TokenStream::from(output)
+}
+
+#[proc_macro_derive(MapperEnc)]
+pub fn derive_mapper_enc(input: TokenStream) -> TokenStream {
+    let mut input: syn::ItemStruct = syn::parse(input).unwrap();
+
+    let decoder_store_impl = input.fields.iter().enumerate().map(|(i, f)| {
+        let name = &f.ident;
+        let ty = &f.ty;
+
+        match ty {
+            syn::Type::Array(array) => {
+                let ty = &array.elem;
+                let len = &array.len;
+                quote! {
+                 self.#name.iter().fold(0, |sum, x| sum + Encode::size(&x) as usize)
+                }
+            }
+            _ => {
+                quote! {
+                     self.#name.size_enc()
+                }
+            }
+        }
+    });
+    let encoder_encode_impl = input.fields.iter().enumerate().map(|(i, f)| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        match ty {
+            syn::Type::Array(array) => {
+                let ty = &array.elem;
+                let len = &array.len;
+                quote! {
+                    for item in &self.#name {
+                        item.encode(buff);
+                    }
+                }
+            }
+            _ => {
+                quote! {
+                    let a_size = <#ty as Encode<'b, Encdr>>::size(&self.#name);
+                    let (target, encoder) = encoder.split_at_mut(a_size);
+                    let #name = <#ty as Encode<'b, Encdr>>::encode(&self.#name, target);
+                    self.#name.encode(buff);
+                }
+            }
+        }
+    });
+    let name = &input.ident;
+    let attrs = &input.attrs;
+    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+
+    let output = quote! {
+        //impl #impl_generics Encoder for #name #ty_generics #where_clause {
+        impl<'a, Data, Encdr: Encoder<'a, Data = Data>>  Encode<'a, Encdr> for #name #ty_generics #where_clause {
+
+            #[inline]
+             fn encode(&self, buff: &'a mut [Data]) {
+                #(
+                   #encoder_encode_impl
+                )*
+            }
+
+            #[inline]
+            fn size_enc(&self) -> usize {
+                let mut size = 0;
+                #(
+                   size += #decoder_store_impl;
+                )*
+                size
+            }
+        }
+    };
+
+    TokenStream::from(output)
+}
---
+++ denc-test/src/main.rs
@@ -0,0 +1,369 @@
+#![feature(test)]
+#![feature(specialization)]
+
+#[cfg(feature = "derive")]
+pub use denc_derive::*;
+
+use denc::*;
+
+extern crate test;
+use test::Bencher;
+
+use rand::rngs::SmallRng;
+use rand::FromEntropy;
+use rand::Rng;
+use rand::SeedableRng;
+
+#[derive(MapperDec)]
+pub struct TestStruct {
+    pub a: u16,
+    pub b: u32,
+}
+
+#[derive(MapperDec)]
+pub struct TestStructSmall {
+    pub a87: u16,
+    pub a32: u32,
+    pub a35: u32,
+    pub a23: u16,
+    pub a42: u8,
+    pub a41: u8,
+    pub a7: u8,
+    pub a47: u8,
+    pub a53: u16,
+    pub a25: u16,
+    pub a94: u32,
+    pub a37: u32,
+    pub a11: u8,
+    pub a02: u8,
+    pub a52: u16,
+    pub a43: u8,
+    pub a57: u16,
+    pub a82: u16,
+    pub a01: u8,
+    pub a91: u32,
+    pub a62: u32,
+    pub a26: u16,
+    pub a06: u8,
+    pub a24: u16,
+    pub a71: u8,
+    pub a93: u32,
+}
+
+#[derive(MapperDec)]
+pub struct TestStructArray {
+    pub a87: u16,
+    pub a32: u32,
+    pub a35: u32,
+    pub a23: u16,
+    pub a42: u8,
+    pub a41: u8,
+    pub a53: [u32; 10],
+    pub a7: u8,
+    pub a47: u8,
+}
+
+#[derive(MapperDec)]
+pub struct TestStructVec {
+    pub a53: Vec<u32>,
+    pub a87: u16,
+    pub a32: u32,
+    pub a35: u32,
+    pub a23: u16,
+    pub a42: u8,
+    pub a41: u8,
+    pub a7: u8,
+    pub a47: u8,
+}
+
+#[derive(MapperDec)]
+pub struct TestStructLarge {
+    pub a87: u16,
+    pub a32: u32,
+    pub a35: u32,
+    pub a23: u16,
+    pub a42: u8,
+    pub a41: u8,
+    pub a7: u8,
+    pub a47: u8,
+    pub a53: u16,
+    pub a25: u16,
+    pub a94: u32,
+    pub a37: u32,
+    pub a11: u8,
+    pub a02: u8,
+    pub a52: u16,
+    pub a43: u8,
+    pub a57: u16,
+    pub a82: u16,
+    pub a01: u8,
+    pub a91: u32,
+    pub a62: u32,
+    pub a26: u16,
+    pub a06: u8,
+    pub a24: u16,
+    pub a71: u8,
+    pub a93: u32,
+    pub a34: u32,
+    pub a3: u32,
+    pub a04: u8,
+    pub a65: u32,
+    pub a75: u8,
+    pub a66: u32,
+    pub a17: u8,
+    pub a61: u32,
+    pub a27: u16,
+    pub a13: u8,
+    pub a51: u16,
+    pub a63: u32,
+    pub a92: u32,
+    pub a05: u8,
+    pub a9: u32,
+    pub a14: u8,
+    pub a31: u32,
+    pub a73: u8,
+    pub a6: u32,
+    pub a76: u8,
+    pub a64: u32,
+    pub a97: u32,
+    pub a03: u8,
+    pub a74: u8,
+    pub a46: u8,
+    pub a16: u8,
+    pub a22: u16,
+    pub a21: u16,
+    pub a84: u16,
+    pub a86: u16,
+    pub a95: u32,
+    pub a81: u16,
+    pub a2: u16,
+    pub a44: u8,
+    pub a1: u8,
+    pub a83: u16,
+    pub a96: u32,
+    pub a12: u8,
+    pub a5: u16,
+    pub a55: u16,
+    pub a8: u16,
+    pub a33: u32,
+    pub a15: u8,
+    pub a45: u8,
+    pub a54: u16,
+    pub a77: u8,
+    pub a36: u32,
+    pub a0: u8,
+    pub a07: u8,
+    pub a85: u16,
+    pub a72: u8,
+    pub a4: u8,
+    pub a67: u32,
+    pub a56: u16,
+}
+
+fn main() {
+    let mut small_rng = SmallRng::from_entropy();
+    (0..5).for_each(|_| {
+        let mut bytes = vec![0u8; 100];
+        bytes[0] = 10;
+        for b in bytes.iter_mut().skip(8) {
+            *b = small_rng.gen();
+        }
+        (0..10000000).for_each(|_| {
+            test::black_box(&bytes);
+            let mut bytes = LittleEndian(&bytes[..]);
+            let pong: TestStructVec = bytes.decode().unwrap();
+            test::black_box(pong);
+        });
+    });
+    println!("done");
+}
+
+#[derive(MapperDec)]
+pub struct TestStructTinyDerive {
+    pub a: u16,
+    pub b: u8,
+}
+
+#[derive(MapperDec)]
+pub struct TestStructTinyRef<'a> {
+    pub a: u16,
+    pub b: u8,
+    pub c: &'a [u8],
+    pub e: &'a [u8],
+}
+
+#[derive(MapperDec)]
+pub struct TestStructTinyT<T: Clone> {
+    pub a: u16,
+    pub b: T,
+}
+
+#[derive(MapperDec)]
+pub struct TestStructTiny {
+    pub a: u16,
+    pub b: u8,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {}
+
+    #[test]
+    fn test_decode_tiny() {
+        let mut bytes = LittleEndian(&[1u8, 0, 2] as &[u8]);
+        let a: u8 = Decode::decode(&mut bytes).unwrap();
+
+        let mut bytes = LittleEndian(&[1u8, 0, 2] as &[u8]);
+        let a: TestStructTiny = Decode::decode(&mut bytes).unwrap();
+        assert_eq!(a.a, 1);
+        assert_eq!(a.b, 2);
+
+        let mut bytes = LittleEndian(&[1u8, 0, 2, 1, 3] as &[u8]);
+        let a: TestStructTinyRef = Decode::decode(&mut bytes).unwrap();
+        assert_eq!(a.a, 1);
+        assert_eq!(a.b, 2);
+        assert_eq!(a.c, &[1u8, 3u8]);
+    }
+
+    #[test]
+    fn test_encode_tiny_derive() {
+        let mut bytes = LittleEndian(&[1u8, 0, 2] as &[u8]);
+        let a: TestStructTiny = Decode::decode(&mut bytes).unwrap();
+        assert_eq!(a.a, 1);
+        assert_eq!(a.b, 2);
+
+        let mut bytes = LittleEndian(&[1u8, 0, 2] as &[u8]);
+        let a: TestStructTinyDerive = Decode::decode(&mut bytes).unwrap();
+        assert_eq!(a.a, 1);
+        assert_eq!(a.b, 2);
+    }
+
+    #[bench]
+    fn bench_decode_small(b: &mut Bencher) {
+        let mut small_rng = SmallRng::from_entropy();
+        (0..5).for_each(|_| {
+            let mut bytes = vec![0u8; 100];
+            for b in bytes.iter_mut() {
+                *b = small_rng.gen();
+            }
+            b.iter(|| {
+                test::black_box(&bytes);
+                let mut bytes = LittleEndian(&bytes[..]);
+                let mut pong: TestStructSmall = bytes.decode().unwrap();
+                test::black_box(pong);
+            });
+        });
+    }
+
+    #[bench]
+    fn bench_decode_large(b: &mut Bencher) {
+        let mut small_rng = SmallRng::from_entropy();
+        (0..5).for_each(|_| {
+            let mut bytes = vec![0u8; 1024];
+            for b in bytes.iter_mut() {
+                *b = small_rng.gen();
+            }
+            b.iter(|| {
+                test::black_box(&bytes);
+                let mut bytes = LittleEndian(&bytes[..]);
+                let pong: TestStructLarge = bytes.decode().unwrap();
+                test::black_box(pong);
+            });
+        });
+    }
+
+    #[bench]
+    fn bench_decode_array(b: &mut Bencher) {
+        let mut small_rng = SmallRng::from_entropy();
+        (0..5).for_each(|_| {
+            let mut bytes = vec![0u8; 100];
+            for b in bytes.iter_mut() {
+                *b = small_rng.gen();
+            }
+            b.iter(|| {
+                test::black_box(&bytes);
+                let mut bytes = LittleEndian(&bytes[..]);
+                let pong: TestStructArray = bytes.decode().unwrap();
+                test::black_box(pong);
+            });
+        });
+    }
+
+    #[bench]
+    fn bench_decode_vec(b: &mut Bencher) {
+        let mut small_rng = SmallRng::from_entropy();
+        (0..5).for_each(|_| {
+            let mut bytes = vec![0u8; 100];
+            bytes[0] = 10;
+            for b in bytes.iter_mut().skip(8) {
+                *b = small_rng.gen();
+            }
+            b.iter(|| {
+                test::black_box(&bytes);
+                let mut bytes = LittleEndian(&bytes[..]);
+                let pong: TestStructVec = bytes.decode().unwrap();
+                test::black_box(pong);
+            });
+        });
+    }
+
+    #[bench]
+    fn bench_decode_small2(b: &mut Bencher) {
+        let mut small_rng = SmallRng::from_entropy();
+        (0..5).for_each(|_| {
+            let mut bytes = vec![0u8; 100];
+            for b in bytes.iter_mut() {
+                *b = small_rng.gen();
+            }
+            let bytes = &bytes[..] as &[u8];
+            let mut buffer = [0u8; 1024];
+            b.iter(|| {
+                test::black_box(&bytes);
+                let mut bytes = LittleEndianReader::new(&bytes[..], &mut buffer[..]);
+                let pong: TestStructSmall = bytes.decode().unwrap();
+                test::black_box(pong);
+            });
+        });
+    }
+
+    #[bench]
+    fn bench_decode_large2(b: &mut Bencher) {
+        let mut small_rng = SmallRng::from_entropy();
+        (0..5).for_each(|_| {
+            let mut bytes = vec![0u8; 1024];
+            for b in bytes.iter_mut() {
+                *b = small_rng.gen();
+            }
+            let bytes = &bytes[..] as &[u8];
+            let mut buffer = [0u8; 1024];
+            b.iter(|| {
+                test::black_box(&bytes);
+                let mut bytes = LittleEndianReader::new(&bytes[..], &mut buffer[..1024]);
+                let pong: TestStructLarge = bytes.decode().unwrap();
+                test::black_box(pong);
+            });
+        });
+    }
+
+    #[bench]
+    fn bench_decode_array2(b: &mut Bencher) {
+        let mut small_rng = SmallRng::from_entropy();
+        (0..5).for_each(|_| {
+            let mut bytes = vec![0u8; 1024];
+            for b in bytes.iter_mut() {
+                *b = small_rng.gen();
+            }
+            let bytes = &bytes[..] as &[u8];
+            let mut buffer = [0u8; 1024];
+            b.iter(|| {
+                test::black_box(&bytes);
+                let mut bytes = LittleEndianReader::new(&bytes[..], &mut buffer[..1024]);
+                let pong: TestStructArray = bytes.decode().unwrap();
+                test::black_box(pong);
+            });
+        });
+    }
+}
